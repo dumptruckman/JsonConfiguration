3f0c8635b1639df7452bb40321932b78c3446938
Jeremy Wood <farachan@gmail.com>
2017-06-14 01:07:34 -0400

    Added a compatibility layer to support the Set data type.

--- src/main/java/com/dumptruckman/bukkit/configuration/json/JsonConfiguration.java
+++ src/main/java/com/dumptruckman/bukkit/configuration/json/JsonConfiguration.java
@@ -3,16 +3,16 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package com.dumptruckman.bukkit.configuration.json;
 
+import com.dumptruckman.bukkit.configuration.SerializableSet;
+import com.dumptruckman.bukkit.configuration.util.SerializationHelper;
 import net.minidev.json.JSONValue;
 import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.jetbrains.annotations.NotNull;
-import org.yaml.snakeyaml.error.YAMLException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -38,101 +38,13 @@ public class JsonConfiguration extends FileConfiguration {
     @NotNull
     @Override
     public String saveToString() {
-        String dump = JSONValue.toJSONString(buildMap(getValues(false)));
+        String dump = JSONValue.toJSONString(SerializationHelper.serialize(getValues(false)));
 
         if (dump.equals(BLANK_CONFIG)) {
             dump = "";
         }
 
         return dump;
-    }
-
-    /**
-     * Takes a Map and parses through the values, to ensure that, before saving, all objects are as appropriate as
-     * possible for storage in json format.
-     *
-     * Specifically it does the following:
-     *   for Map: calls this method recursively on the Map before putting it in the returned Map.
-     *   for List: calls {@link #buildList(java.util.Collection)} which functions similar to this method.
-     *   for ConfigurationSection: gets the values as a map and calls this method recursively on the Map before putting
-     *       it in the returned Map.
-     *   for ConfigurationSerializable: add the {@link ConfigurationSerialization#SERIALIZED_TYPE_KEY} to a new Map
-     *       along with the Map given by {@link org.bukkit.configuration.serialization.ConfigurationSerializable#serialize()}
-     *       and calls this method recursively on the new Map before putting it in the returned Map.
-     *   for Everything else: stores it as is in the returned Map.
-     */
-    @NotNull
-    private Map<String, Object> buildMap(@NotNull final Map<?, ?> map) {
-        final Map<String, Object> result = new LinkedHashMap<String, Object>(map.size());
-        try {
-            for (final Map.Entry<?, ?> entry : map.entrySet()) {
-                Object value = entry.getValue();
-                if (value instanceof Object[]) {
-                    value = new ArrayList<Object>(Arrays.asList((Object[]) value));
-                }
-                if (value instanceof ConfigurationSection) {
-                    result.put(entry.getKey().toString(), buildMap(((ConfigurationSection) value).getValues(false)));
-                } else if (value instanceof Map) {
-                    result.put(entry.getKey().toString(), buildMap(((Map) value)));
-                } else if (value instanceof List) {
-                    result.put(entry.getKey().toString(), buildList((List) value));
-                } else if (value instanceof ConfigurationSerializable) {
-                    ConfigurationSerializable serializable = (ConfigurationSerializable) value;
-                    Map<String, Object> values = new LinkedHashMap<String, Object>();
-                    values.put(ConfigurationSerialization.SERIALIZED_TYPE_KEY, ConfigurationSerialization.getAlias(serializable.getClass()));
-                    values.putAll(serializable.serialize());
-                    result.put(entry.getKey().toString(), buildMap(values));
-                } else {
-                    result.put(entry.getKey().toString(), value);
-                }
-            }
-        } catch (final Exception e) {
-            LOG.log(Level.WARNING, "Error while building configuration map.", e);
-        }
-        return result;
-    }
-
-    /**
-     * Takes a Collection and parses through the values, to ensure that, before saving, all objects are as appropriate
-     * as possible for storage in json format.
-     *
-     * Specifically it does the following:
-     *   for Map: calls {@link #buildMap(java.util.Map)} on the Map before adding to the returned list.
-     *   for List: calls this method recursively on the List.
-     *   for ConfigurationSection: gets the values as a map and calls {@link #buildMap(java.util.Map)} on the Map
-     *       before adding to the returned list.
-     *   for ConfigurationSerializable: add the {@link ConfigurationSerialization#SERIALIZED_TYPE_KEY} to a new Map
-     *       along with the Map given by {@link org.bukkit.configuration.serialization.ConfigurationSerializable#serialize()}
-     *       and calls {@link #buildMap(java.util.Map)} on the new Map before adding to the returned list.
-     *   for Everything else: stores it as is in the returned List.
-     */
-    private List<Object> buildList(@NotNull final Collection<?> collection) {
-        final List<Object> result = new ArrayList<Object>(collection.size());
-        try {
-            for (Object o : collection) {
-                if (o instanceof Object[]) {
-                    o = new ArrayList<Object>(Arrays.asList((Object[]) o));
-                }
-                if (o instanceof ConfigurationSection) {
-                    result.add(buildMap(((ConfigurationSection) o).getValues(false)));
-                } else if (o instanceof Map) {
-                    result.add(buildMap(((Map) o)));
-                } else if (o instanceof List) {
-                    result.add(buildList((List) o));
-                } else if (o instanceof ConfigurationSerializable) {
-                    ConfigurationSerializable serializable = (ConfigurationSerializable) o;
-                    Map<String, Object> values = new LinkedHashMap<String, Object>();
-                    values.put(ConfigurationSerialization.SERIALIZED_TYPE_KEY, ConfigurationSerialization.getAlias(serializable.getClass()));
-                    values.putAll(serializable.serialize());
-                    result.add(buildMap(values));
-                } else {
-                    result.add(o);
-                }
-            }
-        } catch (Exception e) {
-            LOG.log(Level.WARNING, "Error while building configuration list.", e);
-        }
-        return result;
     }
 
     @Override
@@ -158,7 +70,7 @@ public class JsonConfiguration extends FileConfiguration {
     }
 
     private void convertMapsToSections(@NotNull Map<?, ?> input, @NotNull final ConfigurationSection section) {
-        final Object result = dealWithSerializedObjects(input);
+        final Object result = SerializationHelper.deserialize(input);
         if (result instanceof Map) {
             input = (Map<?, ?>) result;
             for (Map.Entry<?, ?> entry : input.entrySet()) {
@@ -174,54 +86,6 @@ public class JsonConfiguration extends FileConfiguration {
         } else {
             section.set("", result);
         }
-    }
-
-    /**
-     * Parses through the input map to deal with serialized objects a la {@link ConfigurationSerializable}.
-     *
-     * Called recursively first on Maps and Lists before passing the parsed input over to
-     * {@link ConfigurationSerialization#deserializeObject(java.util.Map)}.  Basically this means it will deserialize
-     * the most nested objects FIRST and the top level object LAST.
-     */
-    private Object dealWithSerializedObjects(@NotNull final Map<?, ?> input) {
-        final Map<String, Object> output = new LinkedHashMap<String, Object>(input.size());
-        for (final Map.Entry<?, ?> e : input.entrySet()) {
-            if (e.getValue() instanceof Map) {
-                output.put(e.getKey().toString(), dealWithSerializedObjects((Map<?, ?>) e.getValue()));
-            }  else if (e.getValue() instanceof List) {
-                output.put(e.getKey().toString(), dealWithSerializedObjects((List<?>) e.getValue()));
-            } else {
-                output.put(e.getKey().toString(), e.getValue());
-            }
-        }
-        if (output.containsKey(ConfigurationSerialization.SERIALIZED_TYPE_KEY)) {
-            try {
-                return ConfigurationSerialization.deserializeObject(output);
-            } catch (IllegalArgumentException ex) {
-                throw new YAMLException("Could not deserialize object", ex);
-            }
-        }
-        return output;
-    }
-
-    /**
-     * Parses through the input list to deal with serialized objects a la {@link ConfigurationSerializable}.
-     *
-     * Functions similarly to {@link #dealWithSerializedObjects(java.util.Map)} but only for detecting lists within
-     * lists and maps within lists.
-     */
-    protected Object dealWithSerializedObjects(@NotNull final List<?> input) {
-        final List<Object> output = new ArrayList<Object>(input.size());
-        for (final Object o : input) {
-            if (o instanceof Map) {
-                output.add(dealWithSerializedObjects((Map<?, ?>) o));
-            } else if (o instanceof List) {
-                output.add(dealWithSerializedObjects((List<?>) o));
-            } else {
-                output.add(o);
-            }
-        }
-        return output;
     }
 
     @Override
@@ -307,5 +171,7 @@ public class JsonConfiguration extends FileConfiguration {
         return loadConfiguration(new EncodedJsonConfiguration(charset), file);
     }
 
-    public JsonConfiguration() { }
+    public JsonConfiguration() {
+        ConfigurationSerialization.registerClass(SerializableSet.class);
+    }
 }
